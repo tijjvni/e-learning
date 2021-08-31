1d60b3fa13f1cd110130ce54c7e7619aa5f75beb
tijjvni <tijjvni@gmail.com>
2021-08-31 11:08:06 +0000

    updates

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
     
-        {{ redirect()->route('login') }}
+        <!-- {{ redirect()->route('login') }} -->
         
         @if (Route::has('login'))
             <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
