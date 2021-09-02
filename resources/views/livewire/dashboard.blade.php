12347f8dffb619c93fdd9ad7091910b0b2aaa97a
tijjvni <tijjvni@gmail.com>
2021-09-02 13:33:41 +0000

    update

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -4,7 +4,7 @@
 
         <div class="max-w-md p-8 sm:flex sm:space-x-6 bg-coolGray-50 text-coolGray-800">
             <div class="flex-shrink-0 w-full mb-6 h-44 sm:h-32 sm:w-32 sm:mb-0">
-                <img src="https://source.unsplash.com/100x100/?portrait" alt="" class="object-cover object-center w-full h-full rounded">
+                <img src="" alt="" class="object-cover object-center w-full h-full rounded">
             </div>
             <div class="flex flex-col space-y-4">
                 <div>
@@ -19,9 +19,7 @@
                         <span class="text-coolGray-600">leroy.jenkins@company.com</span>
                     </span>
                     <span class="flex items-center space-x-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" aria-label="Phonenumber" class="w-4 h-4">
-                            <path fill="currentColor" d="M449.366,89.648l-.685-.428L362.088,46.559,268.625,171.176l43,57.337a88.529,88.529,0,0,1-83.115,83.114l-57.336-43L46.558,362.088l42.306,85.869.356.725.429.684a25.085,25.085,0,0,0,21.393,11.857h22.344A327.836,327.836,0,0,0,461.222,133.386V111.041A25.084,25.084,0,0,0,449.366,89.648Zm-20.144,43.738c0,163.125-132.712,295.837-295.836,295.837h-18.08L87,371.76l84.18-63.135,46.867,35.149h5.333a120.535,120.535,0,0,0,120.4-120.4v-5.333l-35.149-46.866L371.759,87l57.463,28.311Z"></path>
-                        </svg>
+                        <i class="fa fa"
                         <span class="text-coolGray-600">+25 381 77 983</span>
                     </span>
                 </div>
