5102b2a439cde4dcdf54f66cfd56b40aca392dc3
tijjvni <tijjvni@gmail.com>
2021-09-02 13:48:40 +0000

    update

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,30 +1,7 @@
 <div class="w-full">
 
-    <div class="w-full md:w-2/6">
+    <div class="w-full md:w-2/6 bg-red-200">
 
-        <div class="max-w-md p-8 sm:flex sm:space-x-6 bg-coolGray-50 text-coolGray-800">
-            <div class="flex-shrink-0 w-full mb-6 h-44 sm:h-32 sm:w-32 sm:mb-0">
-                <img src="" alt="" class="object-cover object-center w-full h-full rounded">
-            </div>
-            <div class="flex flex-col space-y-4">
-                <div>
-                    <h2 class="text-2xl font-semibold">Leroy Jenkins</h2>
-                    <span class="text-sm text-coolGray-600">General manager</span>
-                </div>
-                <div class="space-y-1">
-                    <span class="flex items-center space-x-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" aria-label="Email address" class="w-4 h-4">
-                            <path fill="currentColor" d="M274.6,25.623a32.006,32.006,0,0,0-37.2,0L16,183.766V496H496V183.766ZM464,402.693,339.97,322.96,464,226.492ZM256,51.662,454.429,193.4,311.434,304.615,256,268.979l-55.434,35.636L57.571,193.4ZM48,226.492,172.03,322.96,48,402.693ZM464,464H48V440.735L256,307.021,464,440.735Z"></path>
-                        </svg>
-                        <span class="text-coolGray-600">leroy.jenkins@company.com</span>
-                    </span>
-                    <span class="flex items-center space-x-2">
-                        <i class="fa fa-user"></i>
-                        <span class="text-coolGray-600">+25 381 77 983</span>
-                    </span>
-                </div>
-            </div>
-        </div>    
     </div>
     <div class="w-full md:w-4/6">
     </div>
