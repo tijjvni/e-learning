68c21602c2ef30bb41f5b4c0efcfe9b7d1c27ac5
tijjvni <tijjvni@gmail.com>
2021-09-02 13:50:18 +0000

    update

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,9 +1,9 @@
 <div class="w-full">
 
-    <div class="w-full md:w-2/6 bg-red-200">
+    <div class="w-full md:w-2/6 bg-red-200 h-24">
 
     </div>
-    <div class="w-full md:w-4/6">
+    <div class="w-full md:w-4/6 bg-blue-200 h-24">
     </div>
 
 </div>
